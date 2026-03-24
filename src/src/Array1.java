9f36938a2a5dec24c4f985fa915c459e05a5050a
sudiptapanda2004 <sanjupanda2003@gmail.com>
2026-03-24 17:38:36 +0530

    First commit: Initializing DSA repository

---
+++ src/src/Array1.java
@@ -0,0 +1,16 @@
+void main() {
+    int marks[] = new int[100];
+
+    Scanner sc = new Scanner(System.in);
+
+        marks[0] = sc.nextInt();
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Phy : "+marks[0]);
+        System.out.println("Chem : "+marks[1]);
+        System.out.println("Maths : "+marks[2]);
+
+
+}

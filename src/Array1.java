a04ab8e69109e9dde9850a3f8b7db73e6b592b93
sudiptapanda2004 <sanjupanda2003@gmail.com>
2026-03-26 19:37:31 +0530

    Initial commit: Added DSA folder

---
+++ src/Array1.java
@@ -0,0 +1,17 @@
+void main() {
+<<<<<<< HEAD
+    int marks[] = new int[100];
+
+    Scanner sc = new Scanner(System.in);
+
+    marks[0] = sc.nextInt();
+    marks[1] = sc.nextInt();
+    marks[2] = sc.nextInt();
+
+    System.out.println("Phy : "+marks[0]);
+    System.out.println("Chem : "+marks[1]);
+    System.out.println("Math : "+marks[2]);
+=======
+    System.out.print("Hello World");
+>>>>>>> 717a0817a10382eaf3359ca58d459634a5b8bea0
+}
